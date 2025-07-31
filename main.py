80993cd9d9dc8c8d9fa6d66cf467ddccb1f13b61
Aldo Camacho <me@aldocamacho.com>
2025-07-30 19:13:18 -0700

    take path to text file as command line argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words, 
     get_char_count, 
@@ -23,7 +24,11 @@ def report(file_path, num_words, sorted_char_count):
 
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
 
     text = get_book_text(file_path)
     num_words = get_num_words(text)
